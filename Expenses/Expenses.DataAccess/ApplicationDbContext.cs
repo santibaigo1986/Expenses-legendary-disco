f6ccdfbe94ba4f13c5158559e1793580f75b26db
Baigorria Juan <santibaigo1986@gmail.com>
2023-10-22 18:26:37 -0300

    third commit

---
+++ Expenses/Expenses.DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Expenses.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Expenses.DataAccess
+{
+    public class ApplicationDbContext : DbContext 
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<ExpenseModel> ExpensesTable  {  get; set; } 
+    }
+}
